4c437952df9b87d33b38201f5dbb32acc000a719
Gloria Angelica Abolla <114789021+angelicagloriaaa@users.noreply.github.com>
2025-01-08 13:49:11 +0800

    Add files via upload

---
+++ UnitGeometri.pas
@@ -0,0 +1,25 @@
+unit UnitGeometri;
+
+interface
+
+uses
+  Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
+  Dialogs, ExtCtrls, jpeg;
+
+type
+  TMainMenu_1 = class(TForm)
+    Image1: TImage;
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  MainMenu_1: TMainMenu_1;
+
+implementation
+
+{$R *.dfm}
+
+end.
